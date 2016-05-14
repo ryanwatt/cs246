b44aa1243eedd479e3db3a068a4bf69ef7278fef
Ryan Watt <ryanwatt@Ryans-MacBook-Pro-2.local>
2016-05-14 14:10:40 -0600

    added github

--- helloWorld.java
+++ helloWorld.java
@@ -6,5 +6,5 @@ class HelloWorld {
   * Main
   *****************************************************/
 public static void main(String[]args) {
-    system.out.println("Hello World");
+    system.out.println("Hello Github World");
 }
